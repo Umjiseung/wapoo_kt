090f8b819b1695b53cbdf6a081a0b18504adf688
Umjiseung <eom071005123@gmail.com>
2023-12-22 20:37:48 +0900

    add::CorsConfig

---
+++ src/main/kotlin/com/example/wapookt/global/security/config/CorsConfig.kt
@@ -0,0 +1,18 @@
+package com.example.wapookt.global.security.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("*") // 모든 origin 허용
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Authorization", "Content-Type")
+            .allowCredentials(true)
+            .maxAge(3600)
+    }
+}
